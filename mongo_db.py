476b048e82dc423cd6d0ebe5c646d62073c31ff6
vishnuvasan <vishnuvasan@vishnuvasan.com>
2014-09-07 10:57:50 +0530

    Added the Table Format for the User Entries

--- mongo_db.py
+++ mongo_db.py
@@ -1,5 +1,5 @@
 from pymongo import *
-userentry=dict()
+#userentry=dict()
 
 def create_connection(hostname="localhost",portname=27017):
 	global client
@@ -18,6 +18,7 @@ def add_user(name):
 	existing_user=False
 	user_list=db.collection_names()		
 	
+	name=name.replace(" ","")
 	for i in range(0,len(user_list)):
 		user_list[i]=user_list[i].replace("u'","")
 		user_list[i]=user_list[i].replace("'","")
@@ -27,17 +28,33 @@ def add_user(name):
 	if not existing_user:
 		add_collection(name)
 
-def table_format():
+def table_format(*key):
 	global userentry
+	userentry=dict()
+	for i in range(0,len(key)): 
+		userentry[str(key[i])]=""
+
+def user_data(name,*data):
+	name=name.replace(" ","")
+	current_user=db[name]
+	count=0
+	for key in userentry.keys():
+		userentry[key]=str(data[count])
+		count+=1
+	current_user.insert(userentry)
 	
 userentry={"name":"unknown"}
 
 create_connection()
 create_db("ImageManagement")
+table_format("name","age","sex","place of birth")
+add_user("Rahul Saurav")
+user_data("Rahul Saurav","Rahul Saurav",24,"Male","Bihar")
+
 for i in range(0,100):
 	username="User"+str(i)
 	add_user(username)
-        collection=db[username]
+        #collection=db[username]
 	#collection.insert(userentry)	
 
 
